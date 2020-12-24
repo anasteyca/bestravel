4e1ce32d52848adccc82c41d18ceca59608a3e88
anasteyca <39692438+anasteyca@users.noreply.github.com>
2020-12-24 20:37:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+	let preloader =document.getElementById('preloader');
+	preloader.style.display = 'none';
+};
+
+
+
+const filterBox = document.querySelectorAll('.box');
+
+document.querySelector('nav').addEventListener('click', (event) => {
+
+    if (event.target.tagName !== 'LI') return false;
+    let filterClass = event.target.dataset['f'];
+
+    filterBox.forEach(elem => {
+        elem.classList.remove('hide');
+        if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
+            elem.classList.add('hide');
+        }
+    });
+
+});
